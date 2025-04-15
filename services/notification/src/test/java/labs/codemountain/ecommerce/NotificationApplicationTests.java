e20acf39924b1ef0a95b0f0e41209f5b4245e23b
vincevanderberg <vincevanderberg@gmail.com>
2025-04-15 10:11:03 +0200

    feature(notification): initial commit and project scaffolding

---
+++ services/notification/src/test/java/labs/codemountain/ecommerce/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package labs.codemountain.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
